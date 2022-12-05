04e94fba0c287429ac4d0a05e7413d38adec8d45
Janani-Muralitharan <janani@squashapps.com>
2022-12-05 11:25:27 +0530

    side bar design , Drag and Drop

--- src/components/CommonModules/ButtonOptions/buttons.tsx
+++ src/components/CommonModules/ButtonOptions/buttons.tsx
@@ -1,18 +1,14 @@
-import { useState } from 'react';
-import './buttons.css'
+import { useState } from "react";
+import "./buttons.css";
 // import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import Draggable from 'react-draggable';
-import ReplicateButton from '../ReplicateButton/ReplicateButton';
+import Draggable from "react-draggable";
+import ReplicateButton from "../ReplicateButton/ReplicateButton";
 
-
-
-interface ButtonOptionsProps{
-    name: string,
-    icon: string,
-    id: number,
+interface ButtonOptionsProps {
+  name: string;
+  icon: string;
+  id: number;
 }
-
-  
 
 // const [val,setVal]=useState([]);
 // const handleAdd=()=>{
@@ -26,44 +22,48 @@ interface ButtonOptionsProps{
 // setVal(inputdata)
 // }
 
-const ButtonOptions : React.FC<ButtonOptionsProps> = ({ name , icon}) => {
+const ButtonOptions: React.FC<ButtonOptionsProps> = ({ name, icon }) => {
+  console.log("Buttonname", name);
+  console.log("Buttonicon", icon);
+  const [isDragged, setIsDragged] = useState(false);
 
-    console.log('Buttonname',name);
-    console.log('Buttonicon',icon);
-    const [isDragged, setIsDragged] = useState(false)
+  const onStart = (event: any, data: any) => {
+    console.log("HELLO");
+    console.log({ event });
+    console.log({ data });
+    setIsDragged(true);
+  };
 
-
-    const onStart = (event: any, data: any) => {
-        console.log('HELLO')
-        console.log({event});
-        console.log({data});
-        setIsDragged(true)
-    }
-
-    return (
-        <div>
-            {isDragged && 
-                <div className="rectanglebox">
-                    <img className="imagedesign height" src={icon}></img>
-                    <div>{name}</div>
-                </div>
-            }
-            <Draggable
-                onStart={(event, data) => onStart(event, data)}
-                onDrag={(event, data) => { console.log('end') }}
-                onStop={(event, data) => { setIsDragged(true) }}
-            >
-                <div>
-                {isDragged && <ReplicateButton buttonName={name} icon={icon} />}
-                    {!isDragged && <div className="rectanglebox">
-                        <img className="imagedesign height" src={icon}></img>
-                        <div>{name}</div>
-                    </div>}
-                </div>
-            </Draggable>
+  return (
+    <div className={isDragged ? "rectangleMainBox" : ""}>
+      {isDragged && (
+        <div className="rectanglebox">
+          <img className="imagedesign height" src={icon}></img>
+          <div>{name}</div>
         </div>
-    );
-  }
+      )}
+      <Draggable
+        onStart={(event, data) => onStart(event, data)}
+        onDrag={(event, data) => {
+          console.log(data, "123", event);
+        }}
+        onStop={(event, data) => {
+          console.log(event, "456", data);
+          setIsDragged(true);
+        }}
+      >
+        <div>
+          {isDragged && <ReplicateButton buttonName={name} icon={icon} />}
+          {!isDragged && (
+            <div className="rectanglebox">
+              <img className="imagedesign height" src={icon}></img>
+              <div>{name}</div>
+            </div>
+          )}
+        </div>
+      </Draggable>
+    </div>
+  );
+};
 
-  
-  export default ButtonOptions;
+export default ButtonOptions;
--- src/components/CommonModules/ReplicateButton/ReplicateButton.tsx
+++ src/components/CommonModules/ReplicateButton/ReplicateButton.tsx
@@ -1,16 +1,35 @@
-import './ReplicateButton.css'
+import "./ReplicateButton.css";
+import React, { useState, useEffect } from "react";
 interface ReplicateButtonOptions {
-    buttonName: string;
-    icon: string;
+  buttonName: string;
+  icon: string;
 }
 
+const ReplicateButton: React.FC<ReplicateButtonOptions> = ({
+  buttonName,
+  icon,
+}) => {
+  const [buttonNames, setButtonsNames] = useState("");
 
-const ReplicateButton: React.FC<ReplicateButtonOptions> = ({ buttonName, icon }) => {
-    return (
-        <div className="designBox">
-            {buttonName}
-        </div>
-    )
-}
+  useEffect(() => {
+    setButtonsNames(buttonName);
+    console.log("buttonName", buttonName);
+  }, [buttonName]);
+
+  return (
+    <div className="designBox">
+      <div className="designBoxSub">
+        <input
+          type="text"
+          value={buttonNames}
+          onChange={(e) => setButtonsNames(e.target.value)}
+          className="ReplicateInputBox"
+        />
+        <p className="ReplicateLable">{buttonName}</p>
+        <p className="apple">...</p>
+      </div>
+    </div>
+  );
+};
 
 export default ReplicateButton;
