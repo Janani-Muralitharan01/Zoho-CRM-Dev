264d76005b518949a1960c2ca491219d9adf3a94
Janani-Muralitharan <janani@squashapps.com>
2023-01-03 15:17:10 +0530

    onclick in navbar

--- src/components/SuperAdmin/createForm/index.tsx
+++ src/components/SuperAdmin/createForm/index.tsx
@@ -1,30 +1,28 @@
 import SideBar from "./sideBar";
 import DropArea from "./dropArea";
 import TopBars from "./topBars";
-import AddInputs from "./addInputs"
+import AddInputs from "./addInputs";
 import Arrow from "../../../assets/arrow.png";
 import "./CreateForm.css";
 import { useState } from "react";
 
 const CreateForm = () => {
-  const [show,setshow] = useState("")
+  const [show, setshow] = useState("");
 
-  const Atclick = (data:any) =>{
-    console.log("data",data)
-    setshow(data)
-  }
+  const Atclick = (data: any) => {
+    setshow(data);
+  };
   return (
     <div className="mx-3">
       <div className="flex align-items-center justify-content-start">
-        
         <h2 className="create_form_main_head ml-2">Create Form</h2>
       </div>
       <div>
-      <TopBars onclick={Atclick}/></div>
+        <TopBars onclick={Atclick} />
+      </div>
       <div className="flex mt-3 create_form_main">
         <SideBar />
-        {/* {show =="addinputs"?:<DropArea />} */}
-        <AddInputs/>
+        {show == "addinputs" ? <AddInputs /> : <DropArea />}
       </div>
     </div>
   );
--- src/components/SuperAdmin/createForm/topBars.tsx
+++ src/components/SuperAdmin/createForm/topBars.tsx
@@ -3,28 +3,43 @@ import Edit from "../../../assets/edit.png";
 import Add from "../../../assets/add.png";
 import Actions from "../../../assets/actions.png";
 
-const TopBars = ( props:any) => {
+const TopBars = (props: any) => {
   return (
     <div className="topBanners_main flex">
-      <div className="flex align-items-center" >
+      <div className="flex align-items-center">
         <img src={Add} width={14} height={14} />
-        <p className="ml-1" onClick={props.onclick("addinputs")}> Add Inputs</p>
+        <p className="ml-1" onClick={() => props.onclick("addinputs")}>
+          {" "}
+          Add Inputs
+        </p>
       </div>
-      <div className="flex align-items-center mx-5" >
+      <div
+        className="flex align-items-center mx-5"
+        onClick={() => props.onclick("edit")}
+      >
         <img src={Edit} width={14} height={14} />
         <p className="ml-1">Edit</p>
       </div>
-      <div className="flex align-items-center mx-5">
+      <div
+        className="flex align-items-center mx-5"
+        onClick={() => props.onclick("create")}
+      >
         <img src={Actions} width={14} height={14} />
         <p className="ml-1">Create</p>
       </div>
 
-      <div className="flex align-items-center mr-5">
+      <div
+        className="flex align-items-center mr-5"
+        onClick={() => props.onclick("auickActions")}
+      >
         <img src={Actions} width={14} height={14} />
         <p className="ml-1">Quick Actions</p>
       </div>
 
-      <div className="flex align-items-center">
+      <div
+        className="flex align-items-center"
+        onClick={() => props.onclick("preview")}
+      >
         <img src={Actions} width={14} height={14} />
         <p className="ml-1">Preview</p>
       </div>
