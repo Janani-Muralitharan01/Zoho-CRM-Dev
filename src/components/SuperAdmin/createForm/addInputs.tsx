ca4f99113f3aead7da83e44ab510596e933ad8d3
Janani-Muralitharan <janani@squashapps.com>
2023-01-03 16:13:54 +0530

    create form

--- src/components/SuperAdmin/createForm/addInputs.tsx
+++ src/components/SuperAdmin/createForm/addInputs.tsx
@@ -1,35 +1,34 @@
-
 import { Button } from "primereact/button";
 import { useState } from "react";
-import { InputText } from 'primereact/inputtext';
+import { InputText } from "primereact/inputtext";
 const AddInputs = () => {
-     const [click,setclick] = useState(false);
-     const clickButton =() =>{
-          setclick(!click)
-     }
-     return (
-     <div className="flex w-12">
-          {/* <div className="Add_input_page"> */}
-          {click?
-          <div className="w-12">
-               <div>
-               <div className="Add_input_page"> 
-                    <span>Add Title</span><br/>
-                    <span>
-                         <InputText className="w-7" style={{height: '36px'}}/>
-                    </span>
-               </div></div>
+  const [click, setclick] = useState(false);
+  const clickButton = () => {
+    setclick(!click);
+  };
+  return (
+    <div className="h-full flex justify-content-center align-items-center ">
+      {/* <div className="Add_input_page"> */}
+      {click ? (
+        <div className="">
+          <div>
+            <div className="Add_input_page">
+              <span>Add Title</span>
+              <br />
+              <span>
+                <InputText className="w-7" style={{ height: "36px" }} />
+              </span>
+            </div>
           </div>
-          
-          :
-          
-          <div className="Add_input_page">
-          <Button label="+ Add New Fields" onClick={clickButton}/>
-          <p>or drag fields from the lists</p></div>
-          }
-          </div>
-     );
-   }
-   
-   export default AddInputs;
-   
+        </div>
+      ) : (
+        <div className="Add_input_page">
+          <Button label="+ Add New Fields" onClick={clickButton} />
+          <p>or drag fields from the lists</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddInputs;
